b31269a502d22bfb3e81f2bc294b6aba85b6d9bd
Goku-Naman <gokhrunaman@gmail.com>
2026-02-20 11:42:58 +0530

    Update pythonSnippet.py

--- pythonSnippet.py
+++ pythonSnippet.py
@@ -1,14 +1,7 @@
-# -------------------- FAST INPUT --------------------
-import sys
-import threading
-
 def solve():
-    input = sys.stdin.readline
-    t = int(input().strip())
-    for _ in range(t):
-        n = int(input().strip())
-        arr = list(map(int, input().split()))
-        result = sum(arr) 
-        print(result)
+    n = int(input())
+    a = list(map(int , input().split()))
+    print(a)
+
 if __name__ == "__main__":
     solve()
